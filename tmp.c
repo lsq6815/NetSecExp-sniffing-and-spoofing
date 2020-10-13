60d59c4107faf3e7951b8364afc0de9a2dda250d
LSQ6815 <lsq6815@gmail.com>
2020-10-13 22:44:55 +0800

    remove some tmp files

--- tmp.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-#include <sys/types.h>
-#include "data_format.h"
-int main() {
-    for (u_short uc = 0; uc < (unsigned) 256; uc++) printf("%s %s\n", byte_to_hex_str(uc), byte_to_bin_str(uc));
-}
